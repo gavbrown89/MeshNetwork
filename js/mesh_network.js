a333933f63be1e8f8c927c88041e73d73bbdbf44
Fletcher <aldersonf@hotmail.co.uk>
2018-03-27 15:09:08 +0100

    Error Handling Section

--- js/mesh_network.js
+++ js/mesh_network.js
@@ -5,10 +5,12 @@ function setup() {
     createCanvas(210, 210);
     loadJSON("https://lukenelson.co.uk/wifi-tracking/track.php?callback=eggs", getData, 'jsonp');
 }
+
 /**
  *  Get the data from the URL and display it
  * */
 var dataFeed;
+
 function getData(data) {
     console.log(data);
     dataFeed = data;
@@ -20,7 +22,7 @@ function getData(data) {
 function draw() {
     background('#000000');
     fill('#000000');
-    stroke('#01ff02');
+    stroke('#00ff03');
     strokeWeight(4);
     ellipse(105, 100, 200, 200);
     ellipse(105, 100, 170, 170);
@@ -31,13 +33,19 @@ function draw() {
     ellipse(105, 100, 20, 20);
     line(105, 0, 105, 210);
     line(0, 100, 210, 100);
+
     if (dataFeed) {
-        randomSeed(4);
+        randomSeed(1);
         for (var i = 0; i < dataFeed.length; i++) {
             fill('#ffffff');
             stroke('#ffffff');
             ellipse(random(width), random(height), 10, 10);
 
         }
+
     }
 }
+
+/** ERROR HANDLING **/
+document.write(dataFeed);
+console.log(dataFeed);
